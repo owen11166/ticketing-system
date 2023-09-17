c6b4436c5e66e106d194219e6ca1b6e443e13f9b
owen11166 <109325616+owen11166@users.noreply.github.com>
2023-09-17 14:28:50 +0800

    Initial commit

---
+++ src/test/java/com/owen/ticketingsystem/TicketingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.owen.ticketingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
